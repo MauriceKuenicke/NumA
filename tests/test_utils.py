f0ade470d4a6b9d8d62c5d7a8b686a2b546e06df
Maurice Künicke <m.kuenicke1995@gmail.com>
2021-05-24 14:27:53 +0200

    add test for non axial symmetry

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -4,3 +4,11 @@ def test_isAxialSymmetricY():
     def f(x): return x**2
 
     assert utils.isAxialSymmetricY(f, 2)
+
+
+def test_isNotAxialSymmetricY():
+    from numa import utils
+
+    def f(x): return x + 4
+
+    assert not utils.isAxialSymmetricY(f, 2)
